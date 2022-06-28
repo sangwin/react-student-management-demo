fa3b4a1b5094aefa2fe621e7fcf503a032b3a416
Sangwin Gawande <imsangwin@gmail.com>
2022-06-28 11:27:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename={'/react-student-management'}>
+    {/* <BrowserRouter> */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
